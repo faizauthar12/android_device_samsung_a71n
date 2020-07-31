1312d514e0577aa82b386e0f9d51e1e6d4ee13e4
G4Y-Hub <67488689+G4Y-Hub@users.noreply.github.com>
2020-07-31 15:19:31 +0700

    havoc

--- lineage_a71n.mk
+++ lineage_a71n.mk
@@ -2,7 +2,7 @@
 $(call inherit-product, device/samsung/a71n/full_a71n.mk)
 
 # Inherit some common Lineage stuff
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/havoc/config/common_full_phone.mk)
 
 # Product IDs
 PRODUCT_NAME := lineage_a71n
